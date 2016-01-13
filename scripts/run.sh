2b0bb72487aac22fdd15962f2533c851c948d0d1
Paul Turcotte <pturcottework@gmail.com>
2016-01-12 22:19:28 -0800

    updated run script to use uber-jar and pass expression param properly

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+jar_file=simple.expression.calculator-1.0-SNAPSHOT.jar
 main_class=com.acme.calculator.main.Startup
 log_lvl=debug
 if [ $# -ne 1 ]; then
@@ -6,6 +7,5 @@ if [ $# -ne 1 ]; then
     echo "example: $0 \"let(a, 5, add(a, a))\""
     exit
 fi
-cmd="java $main_class \"$1\""
-echo $cmd
-$cmd
+param=$1
+java -cp .:$jar_file $main_class "$param"
