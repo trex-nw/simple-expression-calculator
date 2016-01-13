eec7cb8ec17fdfe6255addd6a6d70df02bf498f6
Paul Turcotte <pturcottework@gmail.com>
2016-01-13 09:06:34 -0800

    added javadoc to Startup class

--- src/main/java/com/acme/calculator/main/Startup.java
+++ src/main/java/com/acme/calculator/main/Startup.java
@@ -7,11 +7,22 @@ import java.math.MathContext;
 import java.util.Arrays;
 
 /**
+ * Entry point for this program to calculate the value of an expression provided on the command-line.
+ *
+ * The calculated result is printed to stdout.
+ *
+ * Future option: could add more cmd-line parameters to allow passing the desired precision & RoundingMode.
+ *
  * Created by pturcotte on 1/8/16.
  */
 public class Startup {
     private static final Logger logger = LogManager.getLogger(Startup.class);
 
+    /**
+     * main() needed to run this program from the command line.
+     *
+     * @param args command-line arguments passed to this program
+     */
     public static void main(final String[] args) {
         logger.info("main()");
         logger.debug("main(): number of arguments: {}", args.length);
@@ -19,8 +30,6 @@ public class Startup {
 
         // if needed / desired, could first remove all whitespace from the expression string:
         // final String exprWithWhitespaceRemoved = args[0].replaceAll("\\s+", "");
-
-        // Future option: could add more cmd-line parameters to allow passing the desired precision & RoundingMode
 
         if (argCountValid(args)) {
             Calculator parser = new Calculator(MathContext.DECIMAL128);
@@ -31,6 +40,14 @@ public class Startup {
         }
     }
 
+    /**
+     * Verifies that the expected number of command-line arguments were
+     * passed in and prints usage information to stdout if an invalid
+     * number of arguments is present.
+     *
+     * @param args command-line arguments passed to this program
+     * @return boolean - whether expected number of parameters are present
+     */
     static boolean argCountValid(String[] args) {
         boolean proceed = true;
         if (args.length != 1) {
