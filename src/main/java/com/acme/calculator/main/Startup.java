000294cbb7aefb0a0fff3723460248217c2faed8
Paul Turcotte <pturcottework@gmail.com>
2016-01-12 22:20:46 -0800

    cleaned up main(), print usage info if not correct number of arguments, pass first argument as expression to be calculated

--- src/main/java/com/acme/calculator/main/Startup.java
+++ src/main/java/com/acme/calculator/main/Startup.java
@@ -11,37 +11,20 @@ import java.math.MathContext;
 public class Startup {
     private static final Logger logger = LogManager.getLogger(Startup.class);
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        if (args.length != 1) {
+            final String className = Startup.class.getName();
+            System.out.println(String.format("Unexpected number of arguments (%d)", args.length));
+            System.out.println(String.format("usage:\n\tjava %s \"[expression]\" ", className));
+            System.out.println(String.format("example:\n\tjava %s \"let(a,7,add(a,div(10,2)))\"", className));
+            System.exit(-1);
+        }
+
+        // if needed / desired, could first remove all whitespace from the expression string:
+        // final String exprWithWhitespaceRemoved = args[0].replaceAll("\\s+", "");
+
         Calculator parser = new Calculator(MathContext.DECIMAL128);
-        System.out.println(parser.calculate("add(div(1,5), div(1,7))"));
 
-        System.out.println(parser.calculate("add(5, 1)"));
-        System.out.println(parser.calculate("sub(-10, -12)"));
-        System.out.println(parser.calculate("let(a, 5, div(1, 0))"));
-//        System.out.println(parser.calculate("let(b, 5, add(a, a))"));
-        System.out.println(parser.calculate("div(5, 9)"));
-        System.out.println(parser.calculate("let(a, 5, 9)"));
-
-        System.out.println(parser.calculate("div(5, 1)"));
-//        System.out.println(parser.calculate("div(5, 2)"));
-//        System.out.println(parser.calculate("div(5, 13)"));
-//        System.out.println(parser.calculate("div(1, 9)"));
-//        System.out.println(parser.calculate("div(5, 9)"));
-//        System.out.println(parser.calculate("add(0, 0)"));
-//        System.out.println(parser.calculate("add(2147483647, 2147483647)"));
-//        System.out.println(parser.calculate("add(-2147483647, -2147483647)"));
-//        System.out.println(parser.calculate("mult(2147483647, 2147483647)"));
-//        System.out.println(parser.calculate("mult(9903520300447984150353281023, mult(9903520300447984150353281023, 9903520300447984150353281023))").toEngineeringString());
-//        System.out.println(parser.calculate("let(a, 5, add(a, a))"));
-//        System.out.println(parser.calculate("let(a, 5, let(b, mult(a, 10), add(b,a)))"));
-        System.out.println(parser.calculate("let(a, let(b, 10, add(b,b)), let(b, 20, add(a, b)))"));
-
-//        System.out.println(parser.calculate("add(32,15)"));
-//        System.out.println(parser.calculate("sub(101,3)"));
-//        System.out.println(parser.calculate("mult(101,3)"));
-//        System.out.println(parser.calculate("div(101,3)"));
-//        System.out.println(parser.calculate("add(1,mult(2,3))"));
-//        System.out.println(parser.calculate("mult(add(2,2),div(9,3))"));
-        // System.out.println(parser.calculate("let(a,3)"));
+        System.out.println(parser.calculate(args[0]));
     }
 }
